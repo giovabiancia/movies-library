a8aa136e8c19969dd1e2c535a7a1c8e491d2d244
Giovanni Bianciardi <giovannibianciardi@gmail.com>
2023-04-16 12:35:03 +0200

    Componente per tornare a Home

--- src/pages/MovieDetailPage.js
+++ src/pages/MovieDetailPage.js
@@ -1,15 +1,24 @@
 import React from "react";
-import MvNavBar from "../components/base/MvNavBar";
 import MvBackgroundImageContainer from "./../components/base/MvBackgroundImageContainer";
+import MvButtonIcon from "./../components/base/MvButtonIcon";
 
 export default function MovieDetailPage() {
+  const handleGoHome = () => {};
   return (
     <div>
       <MvBackgroundImageContainer
         imageUrl={
           "https://poe.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FpurpleAvatar.d066304c.png&w=48&q=75"
         }
-      ></MvBackgroundImageContainer>
+      >
+        <MvButtonIcon
+          onClick={handleGoHome}
+          iconClass={"fa fa-arrow-left fa-lg  "}
+          css={`
+            margin: 20px;
+          `}
+        ></MvButtonIcon>
+      </MvBackgroundImageContainer>
     </div>
   );
 }
